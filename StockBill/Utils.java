15e88c420104c917291771f5da57ff7dd40d130e
youqi1st <youqi1st@gmail.com>
2016-06-29 21:05:03 +0800

    Create Utils.java

---
+++ StockBill/Utils.java
@@ -0,0 +1,25 @@
+package com.youq.stockbill;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Utils {
+
+	public static void LOG(String Tag, String Msg){
+		System.out.println(Tag + Msg);
+	}
+	
+	public  static BufferedInputStream getResourceInputStream(String resourceName) {
+		
+		try {
+				return new BufferedInputStream(new FileInputStream(resourceName));
+			}
+		catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+	
+		return null;
+	}
+	
+}
